b1c7bfa30077aefb3f90ddfc5615224119276da2
Jack Morris <morriseo94@gmail.com>
2020-05-18 16:04:12 +0100

    first init way too late  half way through A

---
+++ spec/code_clan_student_spec.rb
@@ -0,0 +1,31 @@
+require('minitest/autorun')
+require('minitest/reporters')
+require_relative('../code_clan_student')
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+# ------------------------------------------------------------------------------
+
+class TestStudent < MiniTest::Test
+    
+    def test_name
+        student = Student.new("Jack", "E40")
+        assert_equal("Jack", student.name())
+
+    end
+    def test_cohort
+        student = Student.new("Jack", "E40")
+        assert_equal("E40", student.cohort())
+
+    end
+    def test_set_name
+        student = Student.new("Jack", "E40")
+        student.set_name("Bob")
+        assert_equal("Bob", student.name())
+    end
+    def test_set_cohort
+        student = Student.new("Jack", "E40")
+        student.set_cohort("E30")
+        assert_equal("E30", student.cohort())
+    end
+
+
+end
